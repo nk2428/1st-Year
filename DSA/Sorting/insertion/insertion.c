de6ba3a0846fd8aa18c5133b93dc434001536c27
nk2428 <nidhins1807@gmail.com>
2025-03-03 12:39:09 +0530

    insertion updated

---
+++ DSA/Sorting/insertion/insertion.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+// Function to perform insertion sort
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    // Taking input for the number of elements
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Taking array elements input
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Sorting the array using insertion sort
+    insertionSort(arr, n);
+
+    // Printing the sorted array
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
